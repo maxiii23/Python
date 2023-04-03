5235f8feb3496b3971934a90848af218217e9ab8
maxiii23 <maximiliano.cardenas.hernandez@alumnos.sip.cl>
2023-04-03 08:37:21 -0400

    commit

--- python/fundamentals/fundamentals/file.py
+++ python/fundamentals/fundamentals/file.py
@@ -1,73 +1,74 @@
-num1 = 42 #esta es una variable de numero entero
-num2 = 2.3 
-boolean = True
-string = 'Hello World'
-pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
-person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
-fruit = ('blueberry', 'strawberry', 'banana')
-print(type(fruit))
-print(pizza_toppings[1])
-pizza_toppings.append('Mushrooms')
-print(person['name'])
-person['name'] = 'George'
-person['eye_color'] = 'blue'
-print(fruit[2])
+num1 = 42 #declaración de variables
+num2 = 2.3 #declaración de variables
+boolean = True #declaración de variables
+string = 'Hello World' #declaración de variables
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives'] #List 
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}    # tuplas
+fruit = ('blueberry', 'strawberry', 'banana') #List 
+print(type(fruit)) #    - return
+print(pizza_toppings[1]) #    - return
+pizza_toppings.append('Mushrooms') #add value
+print(person['name']) #    - return
+person['name'] = 'George'  #Data Types
+person['eye_color'] = 'blue' #Data Types
+print(fruit[2]) #    - return
 
-if num1 > 45:
-    print("It's greater")
-else:
-    print("It's lower")
+if num1 > 45: #if 
+    print("It's greater")  #    - return
+else: #else
+    print("It's lower") #    - return
 
-if len(string) < 5:
-    print("It's a short word!")
-elif len(string) > 15:
-    print("It's a long word!")
-else:
-    print("Just right!")
+if len(string) < 5: #if
+    print("It's a short word!") #if
+elif len(string) > 15: #else if
+    print("It's a long word!") #    - return
+else: #else
+    print("Just right!") #    - return 
 
-for x in range(5):
-    print(x)
-for x in range(2,5):
-    print(x)
-for x in range(2,10,3):
-    print(x)
+for x in range(5): #for loop
+    print(x) #    - return
+for x in range(2,5): #for loop
+    print(x)#    - return
+for x in range(2,10,3): #for loop
+    print(x) #    - return
 x = 0
-while(x < 5):
-    print(x)
+while(x < 5):  #while loop
+    print(x) #    - return
     x += 1
 
-pizza_toppings.pop()
-pizza_toppings.pop(1)
+pizza_toppings.pop() #- delete value
+pizza_toppings.pop(1) #- delete value
 
-print(person)
-person.pop('eye_color')
-print(person)
+print(person) #    - return
+person.pop('eye_color') #- delete value
+print(person) #    - return
 
-for topping in pizza_toppings:
-    if topping == 'Pepperoni':
-        continue
+for topping in pizza_toppings:  #for loop
+    if topping == 'Pepperoni': #if
+        continue 
     print('After 1st if statement')
-    if topping == 'Olives':
+    if topping == 'Olives': #if
         break
 
-def print_hello_ten_times():
-    for num in range(10):
-        print('Hello')
+def print_hello_ten_times(): #add value
+    for num in range(10): #for loop
+        print('Hello') #    - return
 
 print_hello_ten_times()
 
 def print_hello_x_times(x):
     for num in range(x):
-        print('Hello')
+        print('Hello') #    - return
 
 print_hello_x_times(4)
 
 def print_hello_x_or_ten_times(x = 10):
     for num in range(x):
-        print('Hello')
+        print('Hello') #    - return
 
-print_hello_x_or_ten_times()
-print_hello_x_or_ten_times(4)
+print_hello_x_or_ten_times() #    - return
+print_hello_x_or_ten_times(4) #    - return
+
 
 
 """
---
+++ python/fundamentals/fundamentals/funciones_basicas_2.py
@@ -0,0 +1,33 @@
+def countdown(a):
+    c = []
+    for b in range(a, -1, -1):
+        c.append(b)
+    return c
+print(countdown(10))
+
+#Imprimir y devolver
+
+def imp_dev(a,b):
+    print(a)
+    return b
+imp_dev(7,5)
+
+#Primero más longitud
+def suma_longitud(lista):
+    return lista[0] + len(lista)
+print(suma_longitud([1,2,3,4,5,5,3,6,2]))
+
+#Valores mayores que el segundo
+def filtrar_lista(lista):
+    if len(lista) < 2:
+        return False
+    else:
+        segundo_valor = lista[1] 
+        nueva_lista = [valor for valor in lista if valor > segundo_valor]
+        return nueva_lista
+print(filtrar_lista([3,4,1,5,2,35,6,2,34,54,2,9,76,4,1]))   
+
+#Esta longitud, ese valor
+def crear_lista(tamaño, valor):
+    return [valor] * tamaño
+print(crear_lista(33,8))
